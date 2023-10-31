506f96e7146de2accf6abfe0e51925858d7cbd49
Daniel Oaks <daniel@danieloaks.net>
2023-10-31 14:33:52 +1000

    Start setting up GrsActor

--- addons/godot_response_system/grs.gd
+++ addons/godot_response_system/grs.gd
@@ -1,2 +1,8 @@
 extends Node
 class_name GRS
+
+func add_actor(actor: GrsActor):
+	print_debug("TODO: add actor ", actor.actorName)
+
+func remove_actor(actor: GrsActor):
+	print_debug("TODO: remove actor ", actor.actorName)
--- addons/godot_response_system/grs_actor.gd
+++ addons/godot_response_system/grs_actor.gd
@@ -4,6 +4,9 @@ class_name GrsActor
 ## Emitted when a new response is ready from GRS.
 signal response(type: String, content: String)
 
+## How we refer to this actor in the rules database.
+@export var actorName := ""
+
 @export_group("Idle barks")
 
 ## Idle barks ask the response system whether we have any incidental responses to fire.
@@ -18,6 +21,46 @@ signal response(type: String, content: String)
 ## Randomly add this amount of time to each period.
 @export var idleJitter: float = 0.5
 
+var grs: GRS
+var _idle_timer: Timer
+
+func _enter_tree():
+	# create idle timer
+	_idle_timer = Timer.new()
+	add_child(_idle_timer)
+	_idle_timer.one_shot = true
+	_idle_timer.timeout.connect(emit_idle)
+
+	# start barking
+	if dispatchBarks:
+		# for the first time to wait, make it totally random
+		var time_to_wait = randf() * idle_wait_time()
+		_idle_timer.start(time_to_wait)
+
+	# add self to GRS
+	grs = get_node("/root/GodotResponseSystem")
+	grs.add_actor(self)
+
+func _exit_tree():
+	# remove self from GRS
+	grs.remove_actor(self)
+
+	# stop idle timer
+	_idle_timer.stop()
+
+## Seconds to wait before next idle bark.
+func idle_wait_time() -> float:
+	var nextWaitTime := idlePeriod
+	nextWaitTime += randf() * idleJitter
+	return nextWaitTime
+
+func emit_idle():
+	# dispatch idle event to GRS
+	dispatch(idleConcept)
+
+	# start the next run
+	_idle_timer.start(idle_wait_time())
+
 ## Sends an event to GRS.
 func dispatch(concept: String):
-	pass
+	print_debug("Dispatching concept from ", actorName, " to GRS: ", concept)
